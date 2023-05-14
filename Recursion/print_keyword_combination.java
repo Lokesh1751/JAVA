619a25af52bb6af891d88581a19d9726f687ec39
Lokesh Kumar <101688217+Lokesh1751@users.noreply.github.com>
2023-05-14 17:58:30 +0530

    Create print_keyword_combination.java

---
+++ Recursion/print_keyword_combination.java
@@ -0,0 +1,19 @@
+public class printkeywordcombination {
+    public static String []mapping={".","abc","def","ghi","jkl","mno","pqrs","tu","vwx","yz"};
+    public static void print(String str,int i,String combination){
+        if(i==str.length()){
+            System.out.println(combination);
+            return;
+        }
+        char currchr=str.charAt(i);
+            String m=mapping[currchr-'0'];
+            for(int j=0;j<m.length();j++){
+                print(str, i+1, combination+m.charAt(j));
+            }
+    }
+    public static void main(String[] args){
+        String str="23";
+        print(str, 0, "");
+
+    }
+}
