9eed31fe6227d0650b1f329338c25178b9762ff9
Lokesh Kumar <101688217+Lokesh1751@users.noreply.github.com>
2023-05-15 22:32:37 +0530

    Create Reverse_String.java

---
+++ Strings/Reverse_String.java
@@ -0,0 +1,9 @@
+public class reversestr{
+    public static void main(String[] args){
+        //Write a Java program to reverse a given string using StringBuilder
+
+        StringBuilder sc=new StringBuilder("sujal");
+        sc.reverse();
+        System.out.println(sc);
+    }
+}
