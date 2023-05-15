7d12a5f7afd6a01bb2f8da080f83104e7a5546e6
Lokesh Kumar <101688217+Lokesh1751@users.noreply.github.com>
2023-05-15 22:38:51 +0530

    Create Concat_String.java

---
+++ Strings/Concat_String.java
@@ -0,0 +1,12 @@
+public class concatstr {
+    // Write a Java program to count the occurrence of a substring in a given string
+    // using StringBuilder.
+    public static void main(String[] args) {
+        String str = "abcd";
+        String ans1 = "efg";
+        String sol=str.concat(ans1);
+        System.out.println(sol);
+        
+        
+    }
+}
