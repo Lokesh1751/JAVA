9ecff789cdef5856ecd759af2074e10701bc42c3
Lokesh Kumar <101688217+Lokesh1751@users.noreply.github.com>
2023-05-15 22:44:51 +0530

    Create insert_in_string.java

---
+++ Strings/insert_in_string.java
@@ -0,0 +1,10 @@
+public class insertion{
+    public static void main(String[] args){
+        //Write a Java program to reverse a given string using StringBuilder
+
+        StringBuilder sc=new StringBuilder("sal");
+
+        sc.insert(1,"uj");
+        System.out.println(sc);
+    }
+}
