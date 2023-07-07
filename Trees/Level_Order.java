50e71af91b225d360d828ae1ededb4ab27316c08
Lokesh Kumar <101688217+Lokesh1751@users.noreply.github.com>
2023-07-07 22:39:35 +0530

    Create Level_Order.java
    Level Order Traversal

---
+++ Trees/Level_Order.java
@@ -0,0 +1,22 @@
+public static void Levelorder(Node root) {
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
+        while (!q.isEmpty()) {
+            Node cr = q.remove();
+            if (cr == null) {
+                System.out.println();
+                if (!q.isEmpty()) {
+                    q.add(null);
+                }
+            } else {
+                System.out.print(cr.data + " ");
+                if (cr.left != null) {
+                    q.add(cr.left);
+                }
+                if (cr.right != null) {
+                    q.add(cr.right);
+                }
+            }
+        }
+    }
