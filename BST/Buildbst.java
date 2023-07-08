d6bd1e4816665b04caa76b233a195c6675fddd83
Lokesh Kumar <101688217+Lokesh1751@users.noreply.github.com>
2023-07-08 18:20:00 +0530

    Create Buildbst.java
    build BST

---
+++ BST/Buildbst.java
@@ -0,0 +1,39 @@
+
+import java.util.*;
+
+ class buildbasic {
+    public static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int val) {
+            this.data = val;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static Node insert(Node root, int val) {
+        if (root == null) {
+            Node newnode = new Node(val);
+            root = newnode;
+            return root;
+        }
+        if (root.data > val) {
+            root.left = insert(root.left, val);
+        } else {
+            root.right = insert(root.right, val);
+        }
+        return root;
+    }
+
+    public static void main(String[] args) {
+        int values[] = { 5, 1, 3, 4, 2, 7 };
+        Node root = null;
+        for (int i = 0; i < values.length; i++) {
+            root = insert(root, values[i]);
+        }
+    System.out.print(root.data);
+    }
+}
