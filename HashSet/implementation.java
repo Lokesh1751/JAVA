b0a02eb9d2b138ea090ace7aac7076feddc57167
Lokesh Kumar <101688217+Lokesh1751@users.noreply.github.com>
2023-07-11 19:10:39 +0530

    Create implementation.java
    Hashset data structure

---
+++ HashSet/implementation.java
@@ -0,0 +1,46 @@
+import java.util.*;
+public class Hashset {
+   public static void main(String[] args) {
+     //OPERATIONS PERFORM IN O(1) TIME COMPLEXITY
+    // DUPLICATE ELEMENTS ARE NOT ALLOWED
+    // ORDER IS NOT MAINTAIN => ELEMENTS GET STORED IN RANDOM ORDER
+
+    Set<Integer> s=new HashSet<Integer>();
+
+    // add element
+    s.add(12);
+    s.add(13);
+    s.add(14);
+    s.add(15);
+    s.add(14);
+    s.add(12);
+    System.out.println(s);
+
+    //remove by value
+    s.remove(15);
+    System.out.println(s);
+
+    // contains a particular element or not => contains function => return true or false
+    System.out.println(s.contains(14));
+
+    //check if set is empty or not => isempty() function => return true or false
+    System.out.println(s.isEmpty());
+
+    // clear function => to remove all elements from set and make it empty
+    s.clear();
+    System.out.println(s);
+    System.out.println(s.isEmpty());
+
+    // size of set
+    System.out.println(s.size());
+   } 
+
+  //iterator
+   Iterator it=s.iterator();
+
+        //next=>gives next value initialy it point to null value whose next is first element of hashset
+        //hasnext=> gives true or false whether it has next value or not
+        while(it.hasNext()!=false){
+            System.out.print(it.next()+" ");
+        }
+}
