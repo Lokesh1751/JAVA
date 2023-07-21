124634c5a62751062f2e306c065625e9f4e1f2bc
Lokesh Kumar <101688217+Lokesh1751@users.noreply.github.com>
2023-07-21 13:09:11 +0530

    Create Detect-Negative-Weight-Cycle.java

---
+++ Graphs/Detect-Negative-Weight-Cycle.java
@@ -0,0 +1,77 @@
+import java.util.*;
+
+public class DetectNegativeCycleloops {
+    static class Edge {
+        int src;
+        int dest;
+        int weight;
+
+        Edge(int src, int dest, int weight) {
+            this.src = src;
+            this.dest = dest;
+            this.weight = weight;
+        }
+    }
+    // Bellmen Ford algo is not able to get a accurate result for negative weight
+    // cycle graphs
+    // Time Complexity => O(V.E)
+
+    public static void bellmen(ArrayList<Edge> graph[], int src, int V) {
+        int dist[] = new int[V];
+        for (int i = 0; i < V; i++) {
+            if (i != src) {
+                dist[i] = Integer.MAX_VALUE;
+            }
+        }
+
+        for (int k = 0; k < V - 1; k++) { // O(V)
+            for (int i = 0; i < V; i++) { // O(E)
+                for (int j = 0; j < graph[i].size(); j++) {
+                    Edge e = graph[i].get(j);
+                    int u = e.src;
+                    int v = e.dest;
+                    int w = e.weight;
+                    if (dist[u] != Integer.MAX_VALUE && dist[u] + w < dist[v]) {
+                        dist[v] = dist[u] + w;
+                    }
+                }
+            }
+
+            // Detect -ve Cycle
+            for (int i = 0; i < V; i++) { // O(E)
+                for (int j = 0; j < graph[i].size(); j++) {
+                    Edge e = graph[i].get(j);
+                    int u = e.src;
+                    int v = e.dest;
+                    int w = e.weight;
+                    if (dist[u] != Integer.MAX_VALUE && dist[u] + w < dist[v]) {
+
+                        System.out.print("Negative Cycle Present");
+
+                    }
+                }
+            }
+
+        }
+        for (int i = 0; i < V; i++) {
+            System.out.print(dist[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int V = 5;
+        ArrayList<Edge> graph[] = new ArrayList[V];
+
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        graph[0].add(new Edge(0, 1, 2));
+        graph[0].add(new Edge(0, 2, 4));
+        graph[1].add(new Edge(1, 2, -4));
+        graph[2].add(new Edge(2, 3, 2));
+        graph[3].add(new Edge(3, 4, 4));
+        graph[4].add(new Edge(4, 1, -10));
+
+        bellmen(graph, 0, V);
+    }
+}
