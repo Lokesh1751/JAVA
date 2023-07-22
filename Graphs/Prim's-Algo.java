60944682edc23034eab99cf64e3ce724f1e16ab0
Lokesh Kumar <101688217+Lokesh1751@users.noreply.github.com>
2023-07-22 16:02:41 +0530

    Create Prim's-Algo.java

---
+++ Graphs/Prim's-Algo.java
@@ -0,0 +1,76 @@
+import java.util.*;
+
+public class PrimsAlgo {
+
+    static class Edge {
+        int src;
+        int dest;
+        int weight;
+
+        Edge(int s, int d, int w) {
+            this.src = s;
+            this.dest = d;
+            this.weight = w;
+        }
+    }
+
+    // This Prim's Algorithm is used to calculate the minimum spanning tree of the
+    // graph
+    // spanning tree is that tree in which when we remove some edges from the graph
+    // still it is connected and edge weight of graph is minimum
+
+    public static class Pair implements Comparable<Pair> {
+        int node;
+        int cost;
+
+        public Pair(int n, int d) {
+            this.node = n;
+            this.cost = d;
+        }
+
+        @Override
+        public int compareTo(Pair p2) {
+            return this.cost - p2.cost;
+        }
+    }
+
+    public static void prims(ArrayList<Edge> graph[], int v) {
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        boolean vis[] = new boolean[v];
+        pq.add(new Pair(0, 0));
+        int finalcost = 0;
+
+        while (!pq.isEmpty()) {
+            Pair curr = pq.remove();
+            if (!vis[curr.node]) {
+                vis[curr.node] = true;
+                finalcost += curr.cost;
+                for (int i = 0; i < graph[curr.node].size(); i++) {
+                    Edge e = graph[curr.node].get(i);
+                    if (!vis[e.dest]) {
+                        pq.add(new Pair(e.dest, e.weight));
+                    }
+                }
+            }
+        }
+        System.out.print("Minimum Spanning Tree Cost is: " + finalcost);
+    }
+
+    public static void main(String[] args) {
+        int v = 4;
+        ArrayList<Edge> graph[] = new ArrayList[v];
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<Edge>();
+        }
+        graph[0].add(new Edge(0, 1, 10));
+        graph[0].add(new Edge(0, 2, 15));
+        graph[0].add(new Edge(0, 3, 30));
+        graph[1].add(new Edge(1, 0, 10));
+        graph[1].add(new Edge(1, 3, 40));
+        graph[2].add(new Edge(2, 0, 15));
+        graph[2].add(new Edge(2, 3, 50));
+        graph[3].add(new Edge(3, 1, 40));
+        graph[3].add(new Edge(3, 2, 50));
+        prims(graph, v);
+    }
+}
